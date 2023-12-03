626f6f332f83847156fdc45561f7c3b9a16f2402
Joshua-Leow <josh.leow.2010@gmail.com>
2023-12-03 16:02:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 # for linux
-#export FLASK_APP=app.py; export PYTHONPATH=.; export FLASK_DEBUG=1;
-#flask run --host=0.0.0.0
+export FLASK_APP=app.py; export PYTHONPATH=.; export FLASK_DEBUG=1;
+flask run --host=0.0.0.0
 
 # for windows
-$env:FLASK_APP = "app.py"
-$env:PYTHONPATH = "."
-$env:FLASK_DEBUG = "1"
-flask run --host=0.0.0.0
+#$env:FLASK_APP = "app.py"
+#$env:PYTHONPATH = "."
+#$env:FLASK_DEBUG = "1"
+#flask run --host=0.0.0.0
